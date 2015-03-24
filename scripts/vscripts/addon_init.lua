c9b641f77f4ededaaf37cb77ef3dad872d625930
Invalid Sytnax <vendivar@gmail.com>
2015-03-23 21:00:36 -0400

    Added in some arrays for auto skill levelup
    Added in some arrays for auto skill levelup

--- scripts/vscripts/addon_init.lua
+++ scripts/vscripts/addon_init.lua
@@ -1387,49 +1387,104 @@ function ITT_GameMode:OnPlayerGainedLevel(event)
 			{"ability_hunter_track"},
 			{"ability_hunter_track"}
 		},
-		--{WARRIOR,}
-		--{TRACKER,}
-		--{JUGGERNAUT,}
-		
+		{WARRIOR,
+			{"ability_hunter_giantswing"},
+			{"ability_hunter_giantswing"},
+			{"ability_hunter_giantswing"}
+		},
+		{TRACKER,
+			{"ability_hunter_greatertrack"},
+			{"ability_hunter_dysenterytrack, ability_hunter_sniff"},
+			{"ability_hunter_hidebeacon, ability_hunter_querybeacon"}
+		},
+		{JUGGERNAUT,
+			{"ability_hunter_stayingpower"},
+			{"ability_hunter_stayingpower"},
+			{"ability_hunter_stayingpower"}
+		},
 		{MAGE,
 			{"ability_mage_swap1","ability_mage_swap2","ability_mage_pumpup","ability_mage_flamespray","ability_mage_negativeblast"},
 			{"ability_mage_reducefood","ability_mage_magefire","ability_mage_depress"},
 			{"ability_mage_metronome"}
 		},
-		--{ELEMENTALIST,}
-		--{HYPNOTIST,}
-		--{DEMENTIA_MASTER,}
-		
+		{ELEMENTALIST,
+			{"ability_mage_swap1","ability_mage_swap2","ability_mage_eruption","ability_mage_splittingfire","ability_mage_defenderenergy"},
+			{"ability_mage_electromagnet","ability_mage_chainlightning","ability_mage_freezingblast"},
+			{"ability_mage_stormearthfire"}
+		},
+		{HYPNOTIST,
+			{"ability_mage_swap1","ability_mage_swap2","ability_mage_hypnosis","ability_mage_dreameater","ability_mage_anger"},
+			{"ability_mage_depressionaura", "ability_mage_depressionorb"},
+			{"ability_mage_jealousy", "ability_mage_seizures", "ability_mage_stupefyingfield"}
+		},
+		{DEMENTIA_MASTER,
+			{"ability_mage_swap1","ability_mage_swap2","ability_mage_metronome","ability_mage_maddeningdischarge"},
+			{"ability_mage_dementiarunes", "ability_mage_activaterunes"},
+			{"ability_mage_dementiasummoning", "ability_mage_darkgate"}
+		},		
 		{PRIEST,
 			{"ability_priest_cureall","ability_priest_pumpup","ability_priest_resistall"},
 			{"ability_priest_swap1","ability_priest_swap2","ability_priest_sprayhealing","ability_priest_pacifyingsmoke"},
 			{"ability_priest_mixheat","ability_priest_mixhealth","ability_priest_mixenergy"}
 		},
-		--{BOOSTER},
-		--{MASTER_HEALER},
-		--{SHAMAN,}
-		
+		{BOOSTER
+			{"ability_priest_pumpup", "ability_priest_lightningshield", "ability_priest_fortitude"},
+			{"ability_priest_trollbattlecall", "ability_priest_spiritlink"},
+			{"ability_priest_mapmagic", "ability_priest_angelicelemental"},			
+		},
+		{MASTER_HEALER
+			{"ability_priest_sprayhealing", "ability_priest_healingwave", "ability_priest_selfpreservation"},
+			{"ability_priest_mixhealth", "ability_priest_rangedheal"},
+			{"ability_priest_mixenergy", "ability_priest_mixheat", "ability_priest_replenish"},
+		},
+		{SHAMAN
+			{"ability_priest_pacifyingsmoke", "ability_priest_spiritwalk", "ability_priest_enhancingherbalbalm"},
+			{"ability_priest_maximumfervor", "ability_priest_painkillerbalm"},
+			{"ability_priest_spiritgate", "ability_priest_increasemetabolism"},
+		,}
 		{BEASTMASTER,
 			{},
 			{},
 			{},
 			{},
-			{}
-		},
-		--{CHICKEN_FORM,}
-		--{PACK_LEADER,}
-		--{SHAPESHIFTER,}
-		
-		{THIEF,
-			{"ability_thief_teleport"},
-			{"ability_thief_teleport"},
 			{},
-			{"ability_thief_teleport"}
 		},
-		--{ESCAPE_ARTIST,}
-		--{CONTORTIONIST,}
-		--{ASSASSIN,}
-		
+		{CHICKEN_FORM,
+			{"ability_beastmaster_shortness"},
+			{},
+			{"ability_beastmaster_fowlplay"},
+			{},
+		},
+		{PACK_LEADER,
+			{"ability_beastmaster_calltobattle"},
+			{},
+			{"ability_beastmaster_empathicrage"},
+		},
+		{SHAPESHIFTER,
+			{"ability_beastmaster_wolfform"},
+			{},
+			{"ability_beastmaster_bearform"},
+		},		
+		{THIEF,
+			{"ability_thief_cloak"},
+			{"ability_thief_cloak"},
+			{"ability_thief_cloak"},
+		},
+		{ESCAPE_ARTIST,
+			{"ability_theif_jump"},
+			{"ability_theif_camoflage"},
+			{"ability_theif_blur"},
+		},
+		{CONTORTIONIST,
+			{"ability_theif_smokestream"},
+			{"ability_theif_teletheif"},
+			{"ability_theif_netherfade"},
+		}
+		{ASSASSIN,
+			{"ability_theif_throwingknife"},
+			{"ability_theif_planningphase"},
+			{"ability_theif_assasinate"},
+		},
 		{SCOUT,
 			{"ability_scout_reveal"},
 			{"ability_scout_reveal"},
