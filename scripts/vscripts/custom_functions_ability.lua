6a503d4395b40f4c1d1c2e1e0d987831360c03ac
Moop <dsrubin@ucdavis.edu>
2015-07-11 22:40:50 -0700

    Bug fixes to Gatherers Item Radar
    Fixed Gatherer's item radar pinging items in players inventories. Also
    fixed tinder having a displayed charge

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -26,38 +26,37 @@ function PingItemInRange(keys)
 
     --PingMap(caster:GetPlayerID(),caster:GetOrigin(),1,1,1)
     --code above for checking your position.
+    print("caster info", caster:GetTeam(), caster:GetOrigin(),range)
+    for _,item in pairs( Entities:FindAllByClassnameWithin("dota_item_drop", caster:GetOrigin(), range)) do
+        local containedItem = item:GetContainedItem()
 
-    for itemName,itemColor in pairs(itemTable) do
-        if itemColor == nil then
+        -- Get item color from table, else default white
+        local itemColor = itemTable[containedItem:GetAbilityName()]
+        if (itemColor == nil) then
             itemColor = "255 255 255"
         end
-
+        -- TODO: ignore raw meat since it is now an item.
+        
+        -- Iterate over item color string and parse into specific values
         local stringParse = string.gmatch(itemColor, "%d+")
-
         --need to divide by 255 to convert to 0-1 scale
         local redVal = tonumber(stringParse())/255
         local greenVal = tonumber(stringParse())/255
         local blueVal = tonumber(stringParse())/255
-
-        print("caster info", caster:GetTeam(), caster:GetOrigin(),range)
-        --FindInSphere(handle startFrom, Vector origin, float maxRadius)
-        local ent = Entities:FindInSphere(nil, caster:GetOrigin(), range)
-
-        while ent ~= nil do
-            if ent:GetName() == itemName then
-                print("pinging", ent, "at", ent:GetAbsOrigin().x, ent:GetAbsOrigin().y, ent:GetAbsOrigin().z)
-                --maybe use CreateParticleForPlayer(string particleName, int particleAttach, handle owningEntity, handle owningPlayer)
-                local thisParticle = ParticleManager:CreateParticle("particles/ui_mouseactions/ping_world.vpcf", PATTACH_ABSORIGIN, ent)
-                ParticleManager:SetParticleControl(thisParticle, 0, ent:GetAbsOrigin())
-                ParticleManager:SetParticleControl(thisParticle, 1, Vector(redVal, greenVal, blueVal))
-                print(itemName, redVal, greenVal, blueVal)
-                ParticleManager:ReleaseParticleIndex(thisParticle)
-                ent:EmitSound("General.Ping")   --may be deafening
-            end
-            ent = Entities:FindInSphere(ent, caster:GetOrigin(), range)
-        end
+ 
+        print("pinging", containedItem:GetAbilityName(), "at", item:GetAbsOrigin().x, item:GetAbsOrigin().y, item:GetAbsOrigin().z)
+        --maybe use CreateParticleForPlayer(string particleName, int particleAttach, handle owningEntity, handle owningPlayer)
+        local thisParticle = ParticleManager:CreateParticle("particles/ui_mouseactions/ping_world.vpcf", PATTACH_ABSORIGIN, item)
+        ParticleManager:SetParticleControl(thisParticle, 0, item:GetAbsOrigin())
+        ParticleManager:SetParticleControl(thisParticle, 1, Vector(redVal, greenVal, blueVal))
+        print(containedItem:GetAbilityName(), redVal, greenVal, blueVal)
+        ParticleManager:ReleaseParticleIndex(thisParticle)
+        item:EmitSound("General.Ping")   --may be deafening
+        print("ping color: ", itemColor)
+        
     end
 end
+
 --[[Checks unit inventory for matching recipes. If there's a match, remove all items and add the corresponding potion
     Matches must have the exact number of each ingredient
     Used for both the Mixing Pot and the Herb Telegatherer]]
