aa5f2f28e76eb482cff388df6063a36561c4b387
Michael Ang <mike_angys@hotmail.com>
2015-10-02 10:30:11 +0800

    Update custom_functions_ability.lua

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -57,6 +57,7 @@ function PingItemInRange(keys)
         --Ping Minimap
         local team = caster:GetTeamNumber()
         MinimapEvent( team, caster, item:GetAbsOrigin().x, item:GetAbsOrigin().y, DOTA_MINIMAP_EVENT_HINT_LOCATION, 3 )
+        GameRules:AddMinimapDebugPoint(id, item:GetAbsOrigin(), 255, 0, 0, 500, 6)
     end
 end
 
