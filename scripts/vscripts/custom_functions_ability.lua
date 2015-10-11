89e9279f524e4cd297587d963862ae235ca1323f
omarchehab98 <omarchehab98@gmail.com>
2015-10-11 16:33:20 +0300

    Created Hunter's Challenge spell
    Note: Tooltips for the spell and it's modifiers do not exist.
    The spell should be fully functional.

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -440,9 +440,35 @@ function EnduranceSuccess(keys)
     caster:SetHealth(caster:GetHealth() + block)
 end
 
+function Challenge( event )
+    local caster = event.caster
+    local target = event.target
+
+    target:SetForceAttackTarget(nil)
+
+    if caster:IsAlive() then
+        local order = 
+        {
+            UnitIndex = target:entindex(),
+            OrderType = DOTA_UNIT_ORDER_ATTACK_TARGET,
+            TargetIndex = caster:entindex()
+        }
+
+        ExecuteOrderFromTable(order)
+    else
+        target:Stop()
+    end
+
+    target:SetForceAttackTarget(caster)
+end
+
+function ChallengeEnd( event )
+    local target = event.target
+
+    target:SetForceAttackTarget(nil)
+end
+
 -- Mage Ability Functons
-
-
 function SpawnMageFire(keys)
     local caster = keys.caster
     local spawnPosition = caster:GetAbsOrigin() + (caster:GetForwardVector() * 150)
