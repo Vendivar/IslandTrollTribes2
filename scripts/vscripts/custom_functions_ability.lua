3e3befb1a0914682863f72f487af027339d5a237
omarchehab98 <omarchehab98@gmail.com>
2015-10-11 19:04:12 +0300

    Created Mage's Jealousy spell
    Particles and sound need revision.
    Spell should work as intended, maybe misunderstood the description.

--- scripts/vscripts/custom_functions_ability.lua
+++ scripts/vscripts/custom_functions_ability.lua
@@ -724,6 +724,41 @@ function DreamEater(keys)
 
 end
 
+function JealousyStart( event )
+    local ability = event.ability
+    local target = event.target
+    ability.target_of_ability = target
+end
+
+function Jealousy( event )
+    local ability = event.ability
+    local target_of_ability = ability.target_of_ability
+    local target = event.target
+
+    target:SetForceAttackTarget(nil)
+
+    if target_of_ability:IsAlive() and target_of_ability ~= target then
+        local order = 
+        {
+            UnitIndex = target:entindex(),
+            OrderType = DOTA_UNIT_ORDER_ATTACK_TARGET,
+            TargetIndex = target_of_ability:entindex()
+        }
+
+        ExecuteOrderFromTable(order)
+    else
+        target:RemoveModifierByName(event.modifier)
+    end
+
+    target:SetForceAttackTarget(target_of_ability)
+end
+
+function JealousyEnd( event )
+    local target = event.target
+    PrintTable(event) 
+    target:SetForceAttackTarget(nil)
+end
+
 function ChainLightning(keys)
     local caster = keys.caster
     local target = keys.target
