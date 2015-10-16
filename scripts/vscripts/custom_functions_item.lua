9b6c67789c9bf56269280edb23e6f47506cb8140
omarchehab98 <omarchehab98@gmail.com>
2015-10-16 20:03:14 +0300

    Created Priest Increase Metabolism and Enhancing Herbal Balm
    Notes: Untested, in theory should be working

--- scripts/vscripts/custom_functions_item.lua
+++ scripts/vscripts/custom_functions_item.lua
@@ -70,13 +70,27 @@ function EatMeatRaw(keys)	--triggers the meat eating channel ability
 	if ability == nil then
 		caster:AddAbility(abilityName)
 		ability = caster:FindAbilityByName( abilityName )
-		ability:SetLevel(1)
-		
+		ability:SetLevel(1)		
 	end
 	print("trying to cast ability ", abilityName)
 	caster:CastAbilityNoTarget(ability, -1)
 	--caster:RemoveAbility(abilityName)
 	--]]
+end
+
+function EatMeat( event )
+	local ability = event.ability
+	local caster = event.caster
+	local heal = event.heal_amount
+
+	if caster:HasModifier("modifier_priest_increasemetabolism") then
+		if ability:GetName() == "ability_item_eat_meat_raw" then
+			caster:Heal(1, caster)
+		else
+			caster:Heal(10, caster)
+		end
+	end
+	caster:Heal(heal, caster)
 end
 
 function MageMasherManaBurn(keys)
