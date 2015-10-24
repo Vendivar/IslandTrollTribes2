a1a5cc9ef61541ae79d2f2c15c25e3373c2e3988
MNoya <martinnoya@gmail.com>
2015-10-24 16:16:21 -0200

    Fixed double include of notifications, added error message function

--- game/islandtrolltribes/scripts/vscripts/mechanics.lua
+++ game/islandtrolltribes/scripts/vscripts/mechanics.lua
@@ -1,4 +1,16 @@
 ------------------------------------------------
+--                  Messages                  --
+------------------------------------------------
+
+function SendErrorMessage( pID, string )
+    Notifications:ClearBottom(pID)
+    Notifications:ClearBottomFromAll()
+    Notifications:Bottom(pID, {text=string, style={color='#E62020'}, duration=2})
+    EmitSoundOnClient("General.Cancel", PlayerResource:GetPlayer(pID))
+end
+
+
+------------------------------------------------
 --            Global item applier             --
 ------------------------------------------------
 function ApplyModifier( unit, modifier_name )
