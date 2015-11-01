ddd55d8f141a41812ace88b1e087b1d6e573898e
MNoya <martinnoya@gmail.com>
2015-11-01 12:35:26 -0200

    Fixing Rest Big/Small not giving energy

--- game/islandtrolltribes/scripts/vscripts/mechanics/mana.lua
+++ game/islandtrolltribes/scripts/vscripts/mechanics/mana.lua
@@ -1,20 +1,23 @@
--- TODO: Add popups.lua
 function RestoreMana(keys)
     local target = keys.target
-    if target == nil then
+    local manaGain = keys.ManaRestored
+    if not target then
         target = keys.caster
     end
-    target:GiveMana(keys.ManaRestored)
+
+    target:GiveMana(manaGain)
+    PopupMana(target, manaGain)
 end
 
 function RemoveMana(keys)
     local target = keys.target
+    local ability = keys.ability
     local manaloss = keys.ManaRemoved
-
-    if target == nil then
+    if not target then
         target = keys.caster
     end
 
     local mana = target:GetMana()
-    target:SetMana(mana-manaloss)
+    target:SpendMana(manaloss, ability)
+    PopupMana(target, -manaloss)
 end
