9f69d1a91f9e6bf855021909eac64e77e970e67a
MNoya <martinnoya@gmail.com>
2015-11-01 15:13:48 -0300

    Fixed Flea Attack Aura
    Improved ability behavior (added debuff, adjusted max level, added
    damage and cooldown ability specials, cast range radius overlay)
    Fixed missing out of game state on the dummy passive which was causing
    it to be found by abilities

--- game/islandtrolltribes/scripts/vscripts/heroes/beastmaster/flea.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/beastmaster/flea.lua
@@ -1,108 +1,52 @@
 function FleaAttack(keys)
     local caster = keys.caster
+    local ability = keys.ability
     local level = caster:GetLevel()
     local casterPosition = caster:GetAbsOrigin()
     local teamnumber = caster:GetTeamNumber()
-    local particle = keys.Particle
-    local cooldown = 0
+    local particleName = "particles/units/heroes/hero_weaver/weaver_base_attack.vpcf"
 
-    -- hero level, cooldown
-    local cooldownValues = {
-        {1, 1.0},
-        {2, 0.95},
-        {3, 0.90},
-        {5, 0.85},
-        {8, 0.80},
-        {13,0.75},
-        {21,0.70},
-        {30,0.70}
-    }
+    local cooldown = ability:GetLevelSpecialValueFor("cooldown", level-1)
 
-    -- get cooldown time
-    for i = 1, #cooldownValues do
-        local stats = cooldownValues[i]
-        if level < stats[1] then
-            break
-        else
-            cooldown = stats[2]
-        end
-    end
-
-    -- only run if flea attack is off cooldown
-    if not caster:HasModifier("modifier_fleacooldown") then
-        local target = nil
+    if ability:IsCooldownReady() then
 
         -- check for valid targets
-        local units = FindUnitsInRadius(teamnumber,
-                                casterPosition,
-                                nil,
-                                400,
-                                DOTA_UNIT_TARGET_TEAM_ENEMY,
-                                DOTA_UNIT_TARGET_ALL,
-                                DOTA_UNIT_TARGET_FLAG_NONE,
-                                FIND_ANY_ORDER,
-                                false)
-        -- select random target
-        local dieRoll = RandomInt(1,#units)
-        local target = units[dieRoll]
+        local units = FindUnitsInRadius(teamnumber, casterPosition, nil, ability:GetCastRange(), DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO, 0, 0, false)
+        
+        if #units > 0 then
+            -- select random target
+            local target = units[RandomInt(1,#units)]
 
-        if target ~= nil then
             -- fire flea attack projectile
             local info =
             {
-                Ability = keys.ability,
+                Ability = ability,
                 Target = target,
                 Source = caster,
-                EffectName = particle,
+                EffectName = particleName,
                 vSpawnOrigin = casterPosition,
                 bProvidesVision = false,
                 bDeleteOnHit = true,
                 bDodgeable = true,
-                flExpireTime = 7.0,
                 iMoveSpeed = 1000
             }
             projectile = ProjectileManager:CreateTrackingProjectile(info)
-            -- track which target has been hit
-            caster.fleaTarget = target
 
-            -- apply cooldown modifier
-            local item = CreateItem("item_fleaattackcooldown_modifier_applier", caster, caster)
-            item:ApplyDataDrivenModifier(caster, caster, "modifier_fleacooldown", {duration=cooldown})
+            -- apply cooldown
+            ability:StartCooldown(cooldown)
         end
     end
 end
 
-function FleaAttackDamage(keys)
+function FleaAttackHit(keys)
     local caster = keys.caster
-    local target = keys.caster.fleaTarget
+    local ability = keys.ability
+    local target = keys.target
     local level = caster:GetLevel()
 
-    local dmg = 0
-    local dps = 0
+    local dmg = ability:GetLevelSpecialValueFor("damage", level-1)
     local dpsDur = 2
-
-    local attackValues = {
-        {1, 12, 3.0},
-        {2, 13, 3.1},
-        {3, 14, 3.2},
-        {5, 15, 3.3},
-        {8, 16, 3.4},
-        {13,19, 3.5},
-        {21,24, 3.6},
-        {30,28, 3.7}
-    }
-
-    -- get damage values
-    for i = 1, #attackValues do
-        local stats = attackValues[i]
-        if level < stats[1] then
-            break
-        else
-            dmg = stats[2]
-            dps = stats[3]
-        end
-    end
-
+    
     -- apply damage
     local damageTable = {
         victim = target,
@@ -113,24 +57,5 @@ function FleaAttackDamage(keys)
     ApplyDamage(damageTable)
 
     -- apply DPS
-    target.caster = caster
-    target.duration = dpsDur
-    target.startTime = GameRules:GetGameTime()
-    target.dps = dps
-    target:SetContextThink("flea_dps", FleaAttackDPS, 1.0)
-end
-
-function FleaAttackDPS(target)
-    if (GameRules:GetGameTime() - target.startTime) >= target.duration then
-        return nil
-    else
-        local damageTable = {
-            victim = target,
-            attacker = target.caster,
-            damage = target.dps,
-            damage_type = DAMAGE_TYPE_MAGICAL
-        }
-        ApplyDamage(damageTable)
-    end
-    return 1.0
+    ability:ApplyDataDrivenModifier(caster, target, "modifier_flea_debuff", {duration = dpsDur})
 end
