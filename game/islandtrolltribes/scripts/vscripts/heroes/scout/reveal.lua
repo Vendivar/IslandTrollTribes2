9912ff6ec51571b8f1d4ea59c48f3fd11cb56fa0
MNoya <martinnoya@gmail.com>
2015-11-04 12:08:19 -0300

    Fixed scout reveal truesight
    Bush scout can only be revealed by true sight, not by Scouts proximity

--- game/islandtrolltribes/scripts/vscripts/heroes/scout/reveal.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/scout/reveal.lua
@@ -2,8 +2,9 @@ function Reveal( event )
     local caster = event.caster
     local ability = event.ability
     local level = ability:GetLevel()
+    if ability:GetAbilityName() == ability_scout_greaterreveal then level = 4 end
     local duration = ability:GetSpecialValueFor("duration")
 
-    local dummy = CreateUnitByName("dummy_caster_reveal"..level, caster:GetAbsOrigin(), false, caster, caster, caster:GetTeamNumber())
+    local dummy = CreateUnitByName("dummy_reveal"..level, caster:GetAbsOrigin(), false, caster, caster, caster:GetTeamNumber())
     Timers:CreateTimer(duration, function() dummy:RemoveSelf() end)
 end
