aaf8b094d0b147de86fb98e97c9420c5df259461
MNoya <martinnoya@gmail.com>
2015-11-07 16:17:06 -0300

    Solved item Stone conflict
    Back to the proper version

--- game/islandtrolltribes/scripts/vscripts/items/stone.lua
+++ game/islandtrolltribes/scripts/vscripts/items/stone.lua
@@ -1,11 +1,14 @@
-function StoneStun(keys)
-    print("Stone Stunned!")
-	local ability = keys
-    local caster = keys.caster
-    local target = keys.target
-    local dur = 7.0 --default duration for anything besides heros
-    if (target:IsHero()) then --if the target's name includes "hero"
-        dur = 1.0   --then we use the hero only duration
-    end
-    target:AddNewModifier(caster, nil, "modifier_persistent_invisibility", {duration = dur})
+function Hide( event )
+    local caster = event.caster
+    local origin = caster:GetAbsOrigin()
+    origin.z = -128
+    local item = event.ability
+
+    caster:DropItemAtPositionImmediate(item, origin)
+    item:GetContainer():SetAbsOrigin(Vector(-8000,-8000,0))
+end
+
+function Remove( event )
+    local item = event.ability
+    item:GetContainer():RemoveSelf()
 end
