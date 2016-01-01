b69796fbd99e9a795942c431bacf7175a04363fd
Invalid Sytnax <vendivar@gmail.com>
2016-01-01 13:57:12 -0500

    Mp, HP Potion and some other changes
    Mp, HP Potion and some other changes

--- game/islandtrolltribes/scripts/vscripts/heroes/thief/jump.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/thief/jump.lua
@@ -5,10 +5,5 @@ function Jump(keys)
     local dummyTarget = CreateUnitByName("dummy_caster_metronome", point, false, nil, nil,DOTA_TEAM_NEUTRALS)
 
     FindClearSpaceForUnit(caster, point, false) 
-    local landParticle = ParticleManager:CreateParticle("particles/custom/jump_land_smoke.vpcf", PATTACH_ABSORIGIN, point)
-   			Timers:CreateTimer(3,
-      function()
-          ParticleManager:DestroyParticle(landParticle, false)
-        end)
 end
 
