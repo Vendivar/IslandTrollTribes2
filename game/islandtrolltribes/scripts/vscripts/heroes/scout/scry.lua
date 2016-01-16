20b69289f779e7a57f442a265912cf5c39b5ce7f
Invalid Sytnax <vendivar@gmail.com>
2016-01-16 14:53:44 -0500

    Fixed scry

--- game/islandtrolltribes/scripts/vscripts/heroes/scout/scry.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/scout/scry.lua
@@ -5,6 +5,8 @@ function ScryActive( keys )
 	local ability_level = ability:GetLevel() - 1    
 	local point = keys.target_points[1]
 
+	local particleName = "particles/items_fx/dust_of_appearance.vpcf"
+
 	-- Ability variables
 	local charge_particle = "particles/custom/spirit_walk_glow.vpcf"
 	local radius = ability:GetLevelSpecialValueFor("radius", ability_level)
@@ -13,5 +15,16 @@ function ScryActive( keys )
 
 	-- Create the vision
 	AddFOWViewer(caster:GetTeamNumber(), point, radius, vision_duration, false)
+    
+    
+
+	-- Particle for team
+	for _, v in pairs( all_heroes ) do
+		if v:GetPlayerID() and v:GetTeam() == caster:GetTeam() then
+			local fxIndex = ParticleManager:CreateParticleForPlayer( particleName, PATTACH_WORLDORIGIN, v, PlayerResource:GetPlayer( v:GetPlayerID() ) )
+			ParticleManager:SetParticleControl( fxIndex, 0, point )
+			ParticleManager:SetParticleControl( fxIndex, 1, Vector(radius,0,radius) )
+		end
+	end
 
 end
