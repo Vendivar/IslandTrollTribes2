4f5988196286cd8bf7f80ab1acbd023869a180e8
Oshan <oshan.deshapriya@gmail.com>
2016-02-04 02:59:33 +0530

    Fix for not granting vision from dummies

--- game/islandtrolltribes/scripts/vscripts/heroes/hunter/track.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/hunter/track.lua
@@ -17,3 +17,18 @@ function TrackUnit(keys)
     dummySpotter.target = target
     dummySpotter:SetContextThink("dummy_spotter_thinker"..dummySpotter:GetEntityIndex(), MoveDummySpotter, 0.1)
 end
+
+function MoveDummySpotter(dummySpotter)
+    if (dummySpotter.target:IsAlive() == false) then
+        print("Creature with dummy spotter died, removing it")
+        dummySpotter:ForceKill(true)
+        return nil
+    end
+    dummySpotter:MoveToPosition(dummySpotter.target:GetAbsOrigin())
+    if (GameRules:GetGameTime() - dummySpotter.startTime) >= dummySpotter.duration then
+        dummySpotter:ForceKill(true)
+        --print("spotter is kill")
+        return nil
+    end
+    return 0.1
+end
