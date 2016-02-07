b2ad6781004bef6eb34384b62aa51667c39e268c
Oshan <oshan.deshapriya@gmail.com>
2016-02-07 18:52:52 +0530

    Updated the transmutable item logic in hatching

--- game/islandtrolltribes/scripts/vscripts/buildings/hatch_egg.lua
+++ game/islandtrolltribes/scripts/vscripts/buildings/hatch_egg.lua
@@ -1,56 +1,56 @@
 function HatchEgg(keys)
-
     local hatchery = keys.caster
     local hero = hatchery:GetOwner()
-    local spawnLocation = Vector(hatchery:GetAbsOrigin().x, hatchery:GetAbsOrigin().y, hatchery:GetAbsOrigin().z + 200)
-    local inventoryDetails = GetInventoryDetails(hatchery)
+    local inventoryDetails = ValidateInventory( GetInventoryDetails( hatchery ) )
     if inventoryDetails.isValid then
-        local selectedBird = SelectBird(inventoryDetails, hero)
-        print("Selected bird: "..selectedBird)
-        local hatchEffect = ParticleManager:CreateParticle("particles/custom/fowl_play.vpcf", PATTACH_CUSTOMORIGIN, hatchery)
-        ParticleManager:SetParticleControl(hatchEffect,0,spawnLocation)
-        local babyHawk = CreateUnitByName(selectedBird, spawnLocation, true, nil, nil, hero:GetTeamNumber())
+        local spawnedBird = SpawnBird(SelectBird(inventoryDetails, hero), hatchery)
+        ImproveAbility(spawnedBird, inventoryDetails)
     else
         SendErrorMessage(hero:GetPlayerOwnerID(), inventoryDetails.errorMessage)
     end
 end
 
-function GetInventoryDetails(hatchery)
 
-    local inventoryDetails = { isValid = false, itemCount = 0, hidesCount = 0, clayItemCount = 0, spiritItemCount = 0, thistleCount = 0, muchroomCount =0, darkRockCount=0, otherItemCount=0, errorMessage="", firstItem ="" }
+function GetInventoryDetails(hatchery)
+    local itemTypes = {
+        {type="hide", decides = "bird_type", selects = "npc_creep_hawk",  count=0 },
+        {type="clay", decides = "bird_type", count=0, selects = "npc_creep_drake_bone", count=0},
+        {type="spirit", decides = "bird_type", count=0, selects = "npc_creep_hawk", count=0},
+        {type="thistle", decides = "bird_type", count=0, selects = "npc_creep_hawk", count=0},
+        {type="mushroom", decides = "bird_type", count=0, selects = "npc_creep_hawk", count=0},
+        {type="rock_dark", decides = "bird_type", count=0, selects = "npc_creep_hawk", count=0},
+        {type="stick", decides = "ability", count=0, incleases = "health", by = "5%", count=0},
+        {type="butsu",decides = "ability", count=0, incleases = "movement_speed", by = "5%", count=0},
+    }
+    local inventoryDetails = { isValid = false, totalItemCount = 0, itemTypes = itemTypes,  errorMessage="", firstItem ="" }
     for i=0,5 do
         local item = hatchery:GetItemInSlot(i)
         if item then
             item = item:GetName()
-            if i==0 then
-                inventoryDetails.firstItem = item
+            for i,itemType in pairs(inventoryDetails.itemTypes) do
+                if string.match(item, itemType.type ) then
+                    itemType.count = itemType.count + 1
+                end
             end
-            if string.match(item, "hide") then
-                inventoryDetails.hidesCount = inventoryDetails.hidesCount + 1
-            elseif string.match(item, "clay") then
-                inventoryDetails.clayItemCount = inventoryDetails.clayItemCount + 1
-            elseif string.match(item, "spirit") then
-                inventoryDetails.spiritItemCount = inventoryDetails.spiritItemCount + 1
-            elseif string.match(item, "thistle") then
-                inventoryDetails.thistleCount = inventoryDetails.thistleCount + 1
-            elseif string.match(item, "mushroom") then
-                inventoryDetails.mushroomCount = inventoryDetails.mushroomCount + 1
-            elseif string.match(item, "rock_dark") then
-                inventoryDetails.darkRockCount = inventoryDetails.darkRockCount + 1
-            else
-                inventoryDetails.otherItemCount = inventoryDetails.otherItemCount + 1
-            end
-            inventoryDetails.itemCount = inventoryDetails.itemCount + 1
+            inventoryDetails.totalItemCount = inventoryDetails.totalItemCount + 1
         end
     end
-    return   ValidateInventory(inventoryDetails)
+    inventoryDetails.firstItem = GetFirstItemofInventory(hatchery)
+    return inventoryDetails
 end
 
-function ValidateInventory(inventoryDetails)
 
-    if inventoryDetails.itemCount == 0  then
+function GetFirstItemofInventory( hatchery )
+    if hatchery:GetItemInSlot(0) then
+        return hatchery:GetItemInSlot(0):GetName()
+    end
+end
+
+
+function ValidateInventory(inventoryDetails)
+    if inventoryDetails.totalItemCount == 0  then
         inventoryDetails.errorMessage = "No items in the hatchery"
-    elseif inventoryDetails.itemCount < 6 then
+    elseif inventoryDetails.totalItemCount < 6 then
         inventoryDetails.errorMessage = "There are no enough items"
     elseif inventoryDetails.firstItem ~= "item_egg_hawk" then
         inventoryDetails.errorMessage = "The first slot in the hatchery must contain a Hawk Egg"
@@ -60,25 +60,54 @@ function ValidateInventory(inventoryDetails)
     return inventoryDetails
 end
 
-function SelectBird(inventoryDetails, hero)
 
-    inventoryDetails.selectedBird = ""
+function SelectBird(inventoryDetails, hero)
+    inventoryDetails.selectedBird = "npc_creep_hawk"
     if hero:GetName() == "npc_dota_hero_lycan" then 
         inventoryDetails.selectedBird = "npc_creep_hawk"
-    elseif inventoryDetails.clayItemCount >= 3 then
-        inventoryDetails.selectedBird = "npc_creep_hawk"
-    elseif inventoryDetails.spiritItemCount >= 3 then
-        inventoryDetails.selectedBird = "npc_creep_hawk"
-    elseif inventoryDetails.spiritItemCount >= 3 then
-        inventoryDetails.selectedBird = "npc_creep_hawk"
-    elseif inventoryDetails.thistleCount >= 3 then
-        inventoryDetails.selectedBird = "npc_creep_hawk"
-    elseif inventoryDetails.thistleCount >= 3 then
-        inventoryDetails.selectedBird = "npc_creep_hawk"
-    elseif inventoryDetails.darkRockCount >= 3 then
-        inventoryDetails.selectedBird = "npc_creep_hawk"
-    else
-        inventoryDetails.selectedBird = "npc_creep_hawk"
+        return  inventoryDetails.selectedBird
     end
-    return inventoryDetails.selectedBird;
+    for  i,itemType in pairs(inventoryDetails.itemTypes) do
+        if ( itemType.decides == "bird_type" and itemType.count >= 3  ) then
+            inventoryDetails.selectedBird = itemType.selects
+        end
+    end
+    return inventoryDetails.selectedBird
+end
+
+
+function SpawnBird(selectedBird, hatchery)
+    local spawnLocation = Vector(hatchery:GetAbsOrigin().x, hatchery:GetAbsOrigin().y, hatchery:GetAbsOrigin().z + 200)
+    local hero = hatchery:GetOwner()
+    local hatchEffect = ParticleManager:CreateParticle("particles/custom/fowl_play.vpcf", PATTACH_CUSTOMORIGIN, hatchery)
+    ParticleManager:SetParticleControl(hatchEffect,0,spawnLocation)
+    local babyHawk = CreateUnitByName(selectedBird, spawnLocation, true, nil, nil, hero:GetTeamNumber())
+    return babyHawk
+end
+
+
+function ImproveAbility(selectedBird, inventoryDetails)
+    local functionList = {}
+
+    functionList["health"] = function (creep, by, itemcount)
+        local newHealthValue = creep:GetHealth()+  math.floor(creep:GetHealth()* (itemcount*by/100.0))
+        creep:SetMaxHealth(newHealthValue)
+        creep:SetHealth(newHealthValue)
+        return creep
+    end
+
+    functionList["movement_speed"] = function(creep, by, itemcount)
+        creep:SetBaseMoveSpeed(creep:GetBaseMoveSpeed()+ math.floor(creep:GetBaseMoveSpeed()* (itemcount*by/100.0)))
+        return creep
+    end
+
+
+    for i,itemType in pairs(inventoryDetails.itemTypes) do
+        if itemType.decides == "ability" and itemType.count > 0 then
+           local by = string.gsub(itemType.by,"%%","") -- Removing percentage sign
+           local f = functionList[itemType.incleases]
+           selectedBird = f( selectedBird, by, itemType.count )
+        end
+    end
+    return selectedBird
 end
