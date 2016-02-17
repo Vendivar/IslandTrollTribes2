4a332e74b9289a6a593181b16b128fce6cefd8f9
Oshan <oshan.deshapriya@gmail.com>
2016-02-17 17:26:43 +0530

    Disco duck's stucking issue fix and small ensnare fixes

--- game/islandtrolltribes/scripts/vscripts/mechanics/ensnare.lua
+++ game/islandtrolltribes/scripts/vscripts/mechanics/ensnare.lua
@@ -12,11 +12,26 @@ end
 
 function RegainMovement( event )
     local target = event.target
-    local attacker = event.caster
     if target.flying and target:IsAlive() then
         target:SetMoveCapability(DOTA_UNIT_CAP_MOVE_FLY)
+        EscapeAttackers( target )
     end
-    if attacker:IsAttacking() then
-       attacker:Stop()
+end
+
+function EscapeAttackers( flyingTarget )
+    local targetPosition = flyingTarget:GetAbsOrigin()
+    local units = FindUnitsInRadius(flyingTarget:GetTeamNumber(),
+        targetPosition,
+        nil,
+        250,
+        DOTA_UNIT_TARGET_TEAM_BOTH,
+        DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_HERO,
+        DOTA_UNIT_TARGET_FLAG_NONE,
+        FIND_ANY_ORDER,
+        false)
+    for _,unit in pairs (units) do
+        if unit:IsAttacking()  and unit:GetAttackTarget():GetEntityIndex() == flyingTarget:GetEntityIndex() then
+            unit:Stop()
+        end
     end
 end
