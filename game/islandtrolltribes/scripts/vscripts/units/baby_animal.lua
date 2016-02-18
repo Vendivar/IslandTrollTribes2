47d77214d9646567f9aba8dc753004e92059a965
Oshan <oshan.deshapriya@gmail.com>
2016-02-18 10:33:20 +0530

    Despawning of baby animals

---
+++ game/islandtrolltribes/scripts/vscripts/units/baby_animal.lua
@@ -0,0 +1,13 @@
+function StartDespawnTimer(keys)
+    local babyAnimal = keys.caster
+    local lifeTime = keys.Lifetime
+    lifeTime = RandomFloat(lifeTime, lifeTime + 60.0)
+--    print("Life time of the spawned baby animal is set to "..lifeTime.." seconds")
+    Timers:CreateTimer(DoUniqueString("baby_animal"), {callback=Despawn, endTime = lifeTime}, babyAnimal)
+end
+
+function Despawn(unit)
+    if unit:IsAlive() then
+        unit:ForceKill(false)
+    end
+end
