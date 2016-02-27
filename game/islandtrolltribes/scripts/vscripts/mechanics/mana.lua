86c4d9fffdbd7455c2f8c6ae1f20c838d27138b3
Oshan <oshan.deshapriya@gmail.com>
2016-02-27 21:18:24 +0530

    Prevented reusing sleep + Small buidling fixes

--- game/islandtrolltribes/scripts/vscripts/mechanics/mana.lua
+++ game/islandtrolltribes/scripts/vscripts/mechanics/mana.lua
@@ -21,3 +21,23 @@ function RemoveMana(keys)
     target:SpendMana(manaloss, ability)
     PopupMana(target, -manaloss)
 end
+
+function RestAndRestoreMana(keys)
+    local target = keys.target
+    local caster = keys.caster
+    local ability = keys.ability
+    local rest_duration = ability:GetSpecialValueFor("rest_duration")
+    local rested_duration = ability:GetSpecialValueFor("rested_duration")
+    local invulnerable_duration = ability:GetSpecialValueFor("invuln_duration")
+    if not target:HasModifier("modifier_rested") then
+        ability:ApplyDataDrivenModifier(caster,target,"modifier_sleep",{duration = rest_duration})
+        ability:ApplyDataDrivenModifier(caster,target,"modifier_rested",{duration = rested_duration})
+        ability:ApplyDataDrivenModifier(caster,target,"modifier_invuln",{duration = invulnerable_duration})
+        RestoreMana(keys)
+    else
+        local playerID = target:GetPlayerID()
+        if playerID then
+            SendErrorMessage(playerID, "#error_already_rested")
+        end
+    end
+end
