bedef631597b72f32f761fbffc281f16bb1d14fd
Oshan <oshan.deshapriya@gmail.com>
2016-02-29 20:37:45 +0530

    Storm earth fire spell fixes

--- game/islandtrolltribes/scripts/vscripts/heroes/mage/storm_earth_fire.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/mage/storm_earth_fire.lua
@@ -2,11 +2,27 @@ function StormEarthFire(event)
     local caster    = event.caster
     local target    = event.target
     local dur       = event.duration
-
     local entangle  = CreateItem( "item_scroll_entangling", caster, caster)
     entangle:ApplyDataDrivenModifier( caster, target, "modifier_scroll_entanglingroots", {duration=dur})
-    local ability = event.stone_throw
+    StoneThrow(event)
+end
+
+function StoneThrow(event)
+    local caster = event.caster
+    local target = event.target
+    local abilityName = event.stone_throw
     local dummy = CreateUnitByName("dummy_caster_stormearthfire", caster:GetAbsOrigin() , false, caster, caster, caster:GetTeam())
-    dummy:CastAbilityOnTarget(target, ability, caster:GetPlayerID())
-    dummy:Kill()
+    dummy:AddAbility(abilityName)
+    local abilityStoneThrow = dummy:FindAbilityByName(abilityName)
+    abilityStoneThrow:SetLevel(1)
+    if abilityStoneThrow:IsFullyCastable() then
+        Timers:CreateTimer(0.1, function()
+            dummy:CastAbilityOnTarget(target,abilityStoneThrow, caster:GetPlayerID())
+            return
+        end)
+        Timers:CreateTimer(1.0, function()
+            dummy:ForceKill(true)
+            return
+        end)
+    end
 end
