4aa267560aca3252176b575b9806186e8dc7a5a5
MNoya <martinnoya@gmail.com>
2016-03-05 17:50:09 -0300

    Fixed minimap showing for enemies (needs testing in multiplayer)

--- game/islandtrolltribes/scripts/vscripts/libraries/modifiers/modifier_minimap.lua
+++ game/islandtrolltribes/scripts/vscripts/libraries/modifiers/modifier_minimap.lua
@@ -1,17 +1,15 @@
 modifier_minimap = class({})
 
-if IsServer() then
-    function modifier_minimap:CheckState()
-        local state = {
-            [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
-            [MODIFIER_STATE_COMMAND_RESTRICTED] = true,
-            [MODIFIER_STATE_INVULNERABLE] = true,
-            [MODIFIER_STATE_UNSELECTABLE] = true,
-            [MODIFIER_STATE_NO_HEALTH_BAR] = true,
-            [MODIFIER_STATE_OUT_OF_GAME] = true,
-            [MODIFIER_STATE_NOT_ON_MINIMAP_FOR_ENEMIES] = true,
-        }
+function modifier_minimap:CheckState()
+    local state = {
+        [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+        [MODIFIER_STATE_COMMAND_RESTRICTED] = true,
+        [MODIFIER_STATE_INVULNERABLE] = true,
+        [MODIFIER_STATE_UNSELECTABLE] = true,
+        [MODIFIER_STATE_NO_HEALTH_BAR] = true,
+        [MODIFIER_STATE_OUT_OF_GAME] = true,
+        [MODIFIER_STATE_NOT_ON_MINIMAP_FOR_ENEMIES] = true,
+    }
 
-        return state
-    end
+    return state
 end
