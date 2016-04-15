3b537b37b4580ab03239e017f233f15a70c2add6
Oshan <oshan.deshapriya@gmail.com>
2016-04-15 22:08:47 +0530

    Fixed #45: Hunter's ensnare ability changes

--- game/islandtrolltribes/scripts/vscripts/mechanics/ensnare.lua
+++ game/islandtrolltribes/scripts/vscripts/mechanics/ensnare.lua
@@ -35,3 +35,58 @@ function EscapeAttackers( flyingTarget )
         end
     end
 end
+
+function ElectroMagnetStart(keys)
+    local caster = keys.caster
+    local target = keys.target
+    local pfx = keys.pfx
+    target.pull_pfx = ParticleManager:CreateParticle(pfx, PATTACH_POINT_FOLLOW, caster)
+    Timers:CreateTimer(DrawEnsnareParticle, target)
+end
+
+function DrawEnsnareParticle(target)
+    local particleLifeTime = 1.0
+    if not target:IsAlive() or not target:HasModifier("modifier_ensnare") then
+        return nil
+    end
+    if HasUnitMoved(target) then
+        if target.ensnareParticle then
+            ParticleManager:DestroyParticle(target.ensnareParticle, false)
+        end
+        target.ensnareParticle = ParticleManager:CreateParticle("particles/units/heroes/hero_meepo/meepo_earthbind.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+    end
+    return particleLifeTime
+end
+
+function HasUnitMoved(target)
+    local hasMoved =  false
+    local currentPosition = target:GetAbsOrigin()
+    local oldPosition = target.oldPosition
+    if not oldPosition or (oldPosition.x ~= currentPosition.x or oldPosition.y ~=  currentPosition.y or oldPosition.z ~=  currentPosition.z) then
+        target.oldPosition = currentPosition
+        hasMoved = true
+    end
+    return hasMoved
+end
+
+function ElectroMagnetThink(keys)
+    local caster = keys.caster
+    local origin = caster:GetAbsOrigin()
+    local target = keys.target
+    local targetPosition = target:GetAbsOrigin()
+    local minimumPullRange = keys.min_range
+    local pullMultiplier = keys.pull_scale
+    local difference = origin - targetPosition
+    if math.sqrt( math.pow(difference.x, 2 ) + math.pow( difference.y, 2 ) ) > minimumPullRange then
+        local displacement = difference:Normalized() * pullMultiplier
+        target:SetAbsOrigin( targetPosition + displacement )
+        FindClearSpaceForUnit( target, target:GetAbsOrigin(), true )
+    end
+    ParticleManager:SetParticleControl(target.pull_pfx, 1, Vector( targetPosition.x, targetPosition.y, targetPosition.z + ( target:GetBoundingMaxs().z - target:GetBoundingMins().z ) / 2 ) )
+end
+
+function ElectroMagnetEnd(keys)
+    local target = keys.target
+    ParticleManager:DestroyParticle(target.pull_pfx, false)
+    ParticleManager:DestroyParticle(target.ensnareParticle, false)
+end
