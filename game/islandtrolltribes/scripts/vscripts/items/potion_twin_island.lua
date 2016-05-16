1c9dce6cafc5717e0248ed6e55e6a8cb17d52aa7
Vendivar <Vendivar@gmail.com>
2016-05-16 13:19:33 -0700

    Tooltip updates twin island fixed

--- game/islandtrolltribes/scripts/vscripts/items/potion_twin_island.lua
+++ game/islandtrolltribes/scripts/vscripts/items/potion_twin_island.lua
@@ -1,61 +1,45 @@
 --Not finished, need to find and ping spirit wards and bosses
 function PotionTwinUse(keys)
-  local caster = keys.caster
-  local range = keys.Range
-  local casterPosition = caster:GetAbsOrigin()
-  local teamnumber = caster:GetTeamNumber()
+    local caster = keys.caster
+    local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
+--    print("Ability cast range: "..ability:GetCastRange())
+    local range = ability:GetCastRange()
+--    print("Ability cast range after mapmagic bonus: "..range)
+    local casterPosition = caster:GetAbsOrigin()
+    local teamnumber = caster:GetTeamNumber()
 
-  local units = FindUnitsInRadius(teamnumber,
-    casterPosition,
-    nil,
-    range,
-    DOTA_UNIT_TARGET_TEAM_ENEMY,
-    DOTA_UNIT_TARGET_HERO,
-    DOTA_UNIT_TARGET_FLAG_NONE,
-    FIND_ANY_ORDER,
-    false)
+	local duration = ability:GetLevelSpecialValueFor("duration", ability_level) 
     
-  local redVal = 0
-  local greenVal = 0
-  local blueVal = 0
-  local color = "white"
+    local units = FindUnitsInRadius(teamnumber, casterPosition, nil, range, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, 0, 0, false)
+    
+    if #units > 0 then
+        for _, unit in pairs(units) do
+            local r = 0
+            local g = 0
+            local b = 0
+            local color = "white"
+            if unit:IsRealHero() then
+                r = 255
+                color = "red"
+            else
+                b = 255
+                color = "blue"
+            end
 
-  for _, unit in pairs(units) do
-    if unit:IsHero() then
-      redVal = 255
-      color = "red"
+            local position = unit:GetAbsOrigin()
+            local thisParticle = ParticleManager:CreateParticleForTeam("particles/custom/ping_world.vpcf", PATTACH_ABSORIGIN, caster, teamnumber)
+            ParticleManager:SetParticleControl(thisParticle, 0, position)
+            ParticleManager:SetParticleControl(thisParticle, 1, Vector(r, g, b))
+
+            local particle = ParticleManager:CreateParticleForTeam("particles/custom/ping_static.vpcf", PATTACH_ABSORIGIN, caster, teamnumber)
+            ParticleManager:SetParticleControl(particle, 0, position)
+            ParticleManager:SetParticleControl(particle, 1, Vector(r, g, b))
+            Timers:CreateTimer(duration, function() ParticleManager:DestroyParticle(particle, true) end)
+
+            PingMap(unit, position, color, teamnumber, duration)
+        end
+
+        EmitSoundOnLocationForAllies(caster:GetAbsOrigin(), "General.Ping", caster)
     end
-
-    local thisParticle = ParticleManager:CreateParticle("particles/ui_mouseactions/ping_world.vpcf", PATTACH_ABSORIGIN, caster:GetOwner())
-    ParticleManager:SetParticleControl(thisParticle, 0, unit:GetAbsOrigin())
-    ParticleManager:SetParticleControl(thisParticle, 1, Vector(redVal, greenVal, blueVal))
-    PingMap(caster:GetPlayerID(),unit:GetAbsOrigin(),color, teamnumber)
-    ParticleManager:ReleaseParticleIndex(thisParticle)
-    unit:EmitSound("General.Ping")   --may be deafening
-  end
-  
-  --So for the spirit ward, I was thinking of changing building itself to be a tower, and then use isTower(), but I had some errors and never got around to it
-    
---  local spiritWards = FindUnitsInRadius(teamnumber,
---    casterPosition,
---    nil,
---    range,
---    DOTA_UNIT_TARGET_TEAM_ENEMY,
---    DOTA_UNIT_TARGET_ALL,
---    DOTA_UNIT_TARGET_FLAG_NONE,
---    FIND_ANY_ORDER,
---    false)
---  
---  for _, spiritWard in pairs(spiritWards) do
---    if spiritWard:isTower() then
---      blueVal = 255
---    
---      local thisParticle = ParticleManager:CreateParticle("particles/ui_mouseactions/ping_world.vpcf", PATTACH_ABSORIGIN, caster:GetOwner())
---      ParticleManager:SetParticleControl(thisParticle, 0, spiritWard:GetAbsOrigin())
---      ParticleManager:SetParticleControl(thisParticle, 1, Vector(redVal, greenVal, blueVal))
---      PingMap(caster:GetPlayerID(),spiritWard:GetAbsOrigin(),redVal,greenVal,blueVal)
---      ParticleManager:ReleaseParticleIndex(thisParticle)
---      spiritWard:EmitSound("General.Ping")   --may be deafening
---    end
---  end
 end
