4d45351a900217009456697abc32d98678187482
Vendivar <Vendivar@gmail.com>
2016-05-18 12:05:45 -0700

    Began fixing shape shifter abilities and toggling

--- game/islandtrolltribes/scripts/vscripts/heroes/beastmaster/shapeshift.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/beastmaster/shapeshift.lua
@@ -21,3 +21,58 @@ function Shapeshift(keys)
         end
     end
 end
+
+
+function WolfForm(event)
+    local hero = event.caster
+    local ability = event.ability
+
+    ToggleOffSet(hero, ability)
+end
+
+function BearForm(event)
+    local hero = event.caster
+    local ability = event.ability
+
+    ToggleOffSet(hero, ability)
+end
+
+function ElkForm(event)
+    local hero = event.caster
+    local ability = event.ability
+
+    ToggleOffSet(hero, ability)
+end
+
+function NormalForm(event)
+    local hero = event.caster
+    local ability = event.ability
+
+    ToggleOffSet(hero, ability)
+end
+
+
+-- Toggle the other abilities
+function ToggleOffSet(hero, ability)
+    local wolf = hero:FindAbilityByName("ability_beastmaster_wolfform")
+    local elk = hero:FindAbilityByName("ability_beastmaster_elkform")
+    local bear = hero:FindAbilityByName("ability_beastmaster_bearform")
+    local normal = hero:FindAbilityByName("ability_beastmaster_normalform")
+
+    if bear and ability ~= bear then
+        ToggleOff(bear)
+    end
+
+    if wolf and ability ~= wolf then
+        ToggleOff(wolf)
+    end
+
+    if elk and ability ~= elk then
+        ToggleOff(elk)
+    end
+    
+    if normal and ability ~= normal then
+        ToggleOff(normal)
+    end
+    
+end
