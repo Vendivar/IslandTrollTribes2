801083fe1e9c989098a11c387df58a96e94c8cd4
Dli42 <Davelid136@gmail.com>
2016-05-20 13:08:30 -0400

    Reworked shapeshifter toggles. Fixed abilities staying after death

--- game/islandtrolltribes/scripts/vscripts/heroes/beastmaster/shapeshift.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/beastmaster/shapeshift.lua
@@ -1,28 +1,3 @@
-function Shapeshift(keys)
-    local caster = keys.caster
-    local form = keys.Form
-
-    local formSkills = {
-        {"Normal",nil,{}},
-        {"Bear","modifier_bearform",{"ability_beastmaster_bash","ability_beastmaster_slam"}},
-        {"Wolf","modifier_wolfform",{"ability_beastmaster_howl","ability_beastmaster_criticalstrike"}},
-        {"Elk","modifier_elkform",{"ability_beastmaster_magicimmunity","ability_beastmaster_ram"}}
-    }
-
-    -- modify skill visibilities, remove modifiers from other forms
-    for _,skillList in pairs(formSkills) do
-        local isVisible = (form == skillList[1])
-        local modifier = skillList[2]
-        if form ~= skillList[1] and modifier ~= nil then
-            caster:RemoveModifierByName(modifier)
-        end
-        for _,skill in pairs(skillList[3]) do
-            SetAbilityVisibility(caster, skill, isVisible)
-        end
-    end
-end
-
-
 function WolfForm(event)
     local hero = event.caster
     local ability = event.ability
