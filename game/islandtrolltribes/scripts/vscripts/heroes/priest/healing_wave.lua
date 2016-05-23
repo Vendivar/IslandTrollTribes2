9049f7daec55c38c928b3c62e9c2e92540443eae
Dli42 <Davelid136@gmail.com>
2016-05-23 00:14:12 -0400

    Fixed #180 made Healing Wave all fancy looking

--- game/islandtrolltribes/scripts/vscripts/heroes/priest/healing_wave.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/priest/healing_wave.lua
@@ -1,22 +1,22 @@
 
 function HealingWave(keys)
     local caster = keys.caster
-    local target = keys.target
+    local targetPosition = keys.target_points[1]
     local teamnumber = caster:GetTeamNumber()
     local bounces = keys.Bounces
     local radius = keys.BounceRadius
     local healing = keys.Healing
     local healFactor = keys.BounceHealingFactor
     local healedUnits = {}
-
     -- heal initial target
-    --target:Heal(healing,caster)
-    --table.insert(healedUnits, target)
     local healingWave = ParticleManager:CreateParticle("particles/units/heroes/hero_dazzle/dazzle_shadow_wave.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
+    ParticleManager:SetParticleControl(healingWave,0,Vector(caster:GetAbsOrigin().x,caster:GetAbsOrigin().y,caster:GetAbsOrigin().z+((caster:GetBoundingMaxs().z - caster:GetBoundingMins().z)/2)))
     ParticleManager:SetParticleControl(healingWave,1,Vector(caster:GetAbsOrigin().x,caster:GetAbsOrigin().y,caster:GetAbsOrigin().z+((caster:GetBoundingMaxs().z - caster:GetBoundingMins().z)/2)))
+    local previousTarget = caster
+    local numBounces = 0
+    Timers:CreateTimer(function()
+        print ("Hello. I'm running immediately and then every second thereafter.")
 
-    for i=0,bounces do
-        targetPosition = caster:GetAbsOrigin()
         local units = FindUnitsInRadius(teamnumber,
                                     targetPosition,
                                     nil,
@@ -29,7 +29,7 @@ function HealingWave(keys)
 
         for _,unit in pairs(units) do
             if unit:IsAlive() then
-                print("unit found")
+                --print("unit found")
                 local alreadyHealed = 0
 
                 for j = 0, #healedUnits do
@@ -39,16 +39,30 @@ function HealingWave(keys)
                 end
 
                 if alreadyHealed == 0 then
-                    local origin = target
-                    target = unit
+                    local origin = targetPosition
+                    local target = unit
                     healing = healing*healFactor
                     target:Heal(healing,caster)
                     table.insert(healedUnits, target)
-                    lightningBolt = ParticleManager:CreateParticle("particles/units/heroes/hero_dazzle/dazzle_shadow_wave.vpcf", PATTACH_ABSORIGIN_FOLLOW, origin)
-                    ParticleManager:SetParticleControl(healingWave,1,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z+((target:GetBoundingMaxs().z - target:GetBoundingMins().z)/2)))
+                    healingWave = ParticleManager:CreateParticle("particles/units/heroes/hero_dazzle/dazzle_shadow_wave.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+                    ParticleManager:SetParticleControl(healingWave,1,Vector(previousTarget:GetAbsOrigin().x,previousTarget:GetAbsOrigin().y,previousTarget:GetAbsOrigin().z+((previousTarget:GetBoundingMaxs().z - previousTarget:GetBoundingMins().z)/2)))
+                    ParticleManager:SetParticleControl(healingWave,0,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z+((target:GetBoundingMaxs().z - target:GetBoundingMins().z)/2)))
+                    --print("heal bounce")
+                    previousTarget = target
+                    if #healedUnits == #units then
+                        healedUnits = {}
+                    end
                     break
                 end
             end
         end
+        numBounces = numBounces + 1
+        print("numBounces " .. numBounces)
+        if numBounces >= bounces then
+            return nil
+        else
+            return 0.2
+        end
     end
+    )
 end
