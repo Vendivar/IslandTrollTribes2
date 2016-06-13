8999af7ba0f92ea663751503946e5cf714fb53dd
MNoya <martinnoya@gmail.com>
2016-06-13 10:48:08 -0300

    Added sonar notification - Resolve #203

--- game/islandtrolltribes/scripts/vscripts/items/sonar.lua
+++ game/islandtrolltribes/scripts/vscripts/items/sonar.lua
@@ -1,39 +1,10 @@
-function SonarCompassUse(keys)
-    local caster = keys.caster
-    local unitTable = keys.UnitTable
-    print("SonarCompassUse")
-    PrintTable(unitTable)
-    local units = FindUnitsInRadius(DOTA_TEAM_BADGUYS,
-                      Vector(0, 0, 0),
-                      nil,
-                      FIND_UNITS_EVERYWHERE,
-                      DOTA_UNIT_TARGET_TEAM_BOTH,
-                      DOTA_UNIT_TARGET_ALL,
-                      DOTA_UNIT_TARGET_FLAG_NONE,
-                      FIND_ANY_ORDER,
-                      false)
-    for _,unit in pairs(units) do
-        for unitName,unitColor in pairs(unitTable) do
-            if unitName == unit:GetUnitName() then
-                if unitColor == nil then
-                    unitColor = "255 255 255"
-                end
-                local stringParse = string.gmatch(unitColor, "%d+")
-            
-                --need to divide by 255 to convert to 0-1 scale
-                local redVal = tonumber(stringParse())/255
-                local greenVal = tonumber(stringParse())/255
-                local blueVal = tonumber(stringParse())/255
+function CompassUse(event)
+    local caster = event.caster
+    local team = caster:GetTeamNumber()
+    local playerID = caster:GetPlayerOwnerID()
+    local playerName = PlayerResource:GetPlayerName(playerID)
+    if playerName == "" then playerName = "Player "..playerID end
 
-                print("pinging", unit:GetUnitName(), "at", unit:GetAbsOrigin().x, unit:GetAbsOrigin().y, unit:GetAbsOrigin().z)
-                --maybe use CreateParticleForPlayer(string particleName, int particleAttach, handle owningEntity, handle owningPlayer)
-                local thisParticle = ParticleManager:CreateParticle("particles/ui_mouseactions/ping_world.vpcf", PATTACH_ABSORIGIN, unit)
-                ParticleManager:SetParticleControl(thisParticle, 0, unit:GetAbsOrigin())
-                ParticleManager:SetParticleControl(thisParticle, 1, Vector(redVal, greenVal, blueVal))
-                print(itemName, redVal, greenVal, blueVal)
-                ParticleManager:ReleaseParticleIndex(thisParticle)
-                unit:EmitSound("General.Ping")   --may be deafening
-            end
-        end
-    end
+    Notifications:BottomToTeam(team, {text=playerName, duration=6, style={["margin-right"]="7px;"}, class="NotificationMessage"})
+    Notifications:BottomToTeam(team, {text="#compass_used", duration=6, continue=true, class="NotificationMessage"})
 end
