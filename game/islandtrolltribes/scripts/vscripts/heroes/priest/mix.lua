45bfd50045d9f2ada680c29ad57199058fc1babb
Vendivar <Vendivar@gmail.com>
2016-10-05 20:49:36 -0600

    Fix Priest Targeting

--- game/islandtrolltribes/scripts/vscripts/heroes/priest/mix.lua
+++ game/islandtrolltribes/scripts/vscripts/heroes/priest/mix.lua
@@ -1,3 +1,18 @@
+
+function MixInit(keys)
+    local caster = keys.caster
+    local target = keys.target
+    local ability = keys.ability
+    if string.find(target:GetUnitName(), "npc_dota_hero_dazzle") then
+        SendErrorMessage(caster:GetPlayerOwnerID(),"#invalid_priest_target")
+        caster:Interrupt()
+        ability:StartCooldown(1.0)
+    else
+    print("else")
+    end
+end
+
+
 function MixHeat(keys)
     local caster = keys.caster
     local target = keys.target
